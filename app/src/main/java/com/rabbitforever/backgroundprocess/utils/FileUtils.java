970211b1c8c2e22794e27b7ee099274f956164a6
12006766g <12006766g@connect.polyu.hk>
2018-04-14 20:37:58 +0800

    enhance fileUtils

--- app/src/main/java/com/rabbitforever/backgroundprocess/utils/FileUtils.java
+++ app/src/main/java/com/rabbitforever/backgroundprocess/utils/FileUtils.java
@@ -5,7 +5,12 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileUtils {
     private static FileUtils fileUtils;
@@ -81,5 +86,90 @@ public class FileUtils {
             Log.e(this.getClass().getSimpleName() + ".isFileExisted()", "exception", e);
         }
         return isFileExisted;
+    }
+    public List<String> getFileList(){
+        List<String> fileList = null;
+        try{
+
+        } catch (Exception e){
+            Log.e(this.getClass().getSimpleName() + ".getFileList()", "exception", e);
+        }
+        return fileList;
+    }
+    public void createDirectoryIfNotExisted(String directoryName) throws Exception {
+        File theDir = null;
+        try {
+            theDir = new File(directoryName);
+
+            if (!theDir.exists()){
+                theDir.mkdirs();
+            }
+
+
+        } catch (Exception e) {
+            Log.e(this.getClass().getSimpleName(),
+                    ".createDirectoryIfNotExisted() -directoryName=" + directoryName, e);
+            throw e;
+        } finally{
+            if (theDir != null){
+                theDir = null;
+            }
+        }
+    }
+    public List<String> readFromFile(String fileName) throws Exception {
+        BufferedReader br = null;
+        List<String> stringList = new ArrayList<String>();
+        try {
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(fileName));
+            while ((sCurrentLine = br.readLine()) != null) {
+                stringList.add(sCurrentLine);
+            }
+
+        } catch (IOException e) {
+            Log.e(this.getClass().getSimpleName() + ".readFromFile()", "exception:  - fileName=" + fileName, e);
+        } finally {
+            try {
+                if (br != null){
+                    br.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return stringList;
+    }
+
+    public void traverseDir(String fileInString, List<File> fileList){
+        try {
+            File file = new File(fileInString);
+            if (file != null){
+                traverseDir(file, fileList);
+            }
+        } catch (Exception e) {
+            Log.e(this.getClass().getSimpleName(),
+                    ".traverseDir() -fileInString=" + fileInString + ",fileList="+ fileList, e);
+            throw e;
+        } finally{
+
+        }
+    }
+    public void traverseDir(File dir, List<File> fileList){
+        try {
+            for (File fileEntry: dir.listFiles()){
+                if (fileEntry.isDirectory()) {
+                    traverseDir(fileEntry, fileList);
+                } else {
+                    fileList.add(fileEntry);
+
+                }
+            }
+        } catch (Exception e) {
+            Log.e(this.getClass().getSimpleName(),
+                    ".traverseDir() -dir=" + dir + ",fileList="+ fileList, e);
+            throw e;
+        } finally{
+
+        }
     }
 }
